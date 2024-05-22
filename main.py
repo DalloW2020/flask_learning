6bcf74c5b12687fc2f7c8e81a8afcf65ce40a746
Dallow2020 <dallasdevco@gmail.com>
2024-05-22 21:13:00 +0800

    Update and Delete from Database

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash,request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Email
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'it-is-a-secret-key-for-test-purposes'
@@ -12,13 +14,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 #init db
 db = SQLAlchemy(app)
 app.app_context()
-
+migrate = Migrate(app, db)
+"""
+Flask migrate commands:
+flask db migrate -m "enter a tag"
+flask db upgrade
+"""
 
 # Create database model
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), nullable=False, unique=True)
+    favorite_color = db.Column(db.String(200))
     date_added = db.Column(db.DateTime, default=datetime.now())
 
     def __repr__(self):
@@ -28,6 +36,7 @@ class Users(db.Model):
 class NameForm(FlaskForm):
     name = StringField('Enter Name', validators=[DataRequired()])
     email = EmailField("Email", validators=[DataRequired(), Email()])
+    favorite_color = StringField('Favorite Color')
     submit = SubmitField('Submit')
 
 #
@@ -67,12 +76,13 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data)
+            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ""
         form.email.data = ""
+        form.favorite_color.data = ""
         flash('User added to DataBase')
     our_users = Users.query.order_by(Users.date_added)
     return render_template('add_user.html',
@@ -80,6 +90,51 @@ def add_user():
                            name=name,
                            our_users=our_users)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    user_to_delete = Users.query.get_or_404(id)
+    form = NameForm()
+    name = None
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash('User Deleted!!')
+        our_users = Users.query.order_by(Users.date_added)
+        return render_template('add_user.html',
+                               form=form,
+                               name=name,
+                               our_users=our_users)
+    except:
+        flash('Seems there was a problem with the delete... Please try again')
+        our_users = Users.query.order_by(Users.date_added)
+        return render_template('add_user.html',
+                               form=form,
+                               name=name,
+                               our_users=our_users)
 
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    user_to_update = Users.query.get_or_404(id)
+    form = NameForm()
+    name = None
+    if request.method == "POST":
+        user_to_update.name = request.form['name']
+        user_to_update.email = request.form['email']
+        user_to_update.favorite_color = request.form['favorite_color']
+        try:
+           db.session.commit()
+           flash('Update Record Successfully')
+           return render_template('update.html',
+                                  form=form,
+                                  user_to_update=user_to_update)
+        except:
+            flash('Update Record Error')
+            return render_template('update.html',
+                                   form=form,
+                                user_to_update=user_to_update)
+    else:
+        return render_template('update.html',
+                               form=form,
+                               user_to_update=user_to_update)
 if __name__ == '__main__':
     app.run(debug=True)
