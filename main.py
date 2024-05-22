12dd5f6c275fa4fc008f03d2234230259d210e7f
Dallow2020 <dallasdevco@gmail.com>
2024-05-22 22:20:48 +0800

    set up for hashing

--- main.py
+++ main.py
@@ -28,6 +28,19 @@ class Users(db.Model):
     email = db.Column(db.String(200), nullable=False, unique=True)
     favorite_color = db.Column(db.String(200))
     date_added = db.Column(db.DateTime, default=datetime.now())
+    # password stuff
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<Name %r>' % self.name
